32bea33568e9798635abeb5418aa283fdc08d9cd
Thiago Luis <thiagoviips@hotmail.com>
2025-01-10 17:48:43 -0400

    Criacao do construtor e sobrecarga

---
+++ Produto.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp35
+{
+    internal class Produto
+    {
+     public string Nome;
+     public string Categoria;
+     public decimal Preco;
+     public int Quantidade;
+
+     public Produto(){
+       Nome = "Produto desconhecido";
+       Categoria = "Sem categoria";
+       Preco = 0.0m;
+       Quantidade = 0;
+
+     }
+     public Produto(string nome,string categoria,decimal preco,int quantidade){
+        Nome = nome;
+        Categoria = categoria;
+        Preco = preco;
+        Quantidade = quantidade;
+
+      }
+
+     public Produto(string nome,string categoria){
+         Nome = nome;
+         Categoria = categoria;
+         Preco = 0.0m;
+         Quantidade = 0;
+
+      }
+     public void ExibirInfo(){
+      Console.WriteLine($"Nome: {Nome}");
+      Console.WriteLine($"Categoria: {Categoria}");
+      Console.WriteLine($"Preco: {Preco.ToString("F2",CultureInfo.InvariantCulture)}");
+      Console.WriteLine($"Quantidade: {Quantidade}");
+     }
+     public void AlterarPreco(decimal preco){
+      Preco = preco;
+      Console.WriteLine($"Novo Preço atualizado de {Preco}");
+            
+     } 
+     public void AdicionarEstoque(int quantidade){
+     Quantidade += quantidade;
+            Console.WriteLine($"{quantidade} unidades adicionadas ao estoque. total em estoque {Quantidade}");
+        
+        
+      }
+
+
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using ConsoleApp35;
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         Produto produto1 = new Produto();
+            produto1.ExibirInfo();
+
+         Console.WriteLine();
+         Produto produto2 = new Produto("TV", "Eletrônico", 2500.00m, 10);
+         produto2.ExibirInfo();
+
+         Console.WriteLine();
+         Produto produto3 = new Produto("Smartphone", "Eletrônico");
+          produto3.ExibirInfo();
+
+         Console.WriteLine();
+         produto3.AlterarPreco(800.09m);
+         Console.WriteLine();
+         produto3.ExibirInfo();
+
+         Console.WriteLine();
+         produto3.AdicionarEstoque(8);
+         Console.WriteLine();
+         produto3.ExibirInfo();
+
+         Console.WriteLine();
+         produto3.AdicionarEstoque(3);
+         Console.WriteLine();
+         produto3.ExibirInfo();
+
+         Console.WriteLine();
+         produto3.AdicionarEstoque(4);
+         Console.WriteLine();
+         produto3.ExibirInfo();
+
+
+        }
+    }
+}
