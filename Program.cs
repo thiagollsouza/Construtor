d2ae7228f0440e0f95cababcaa3c99029f054a70
Thiago Luis <thiagoviips@hotmail.com>
2025-01-18 17:02:38 -0400

    Atualizado Iphone Pro Max

--- Program.cs
+++ Program.cs
@@ -11,11 +11,11 @@ namespace MyApp
             produto1.ExibirInfo();
 
          Console.WriteLine();
-         Produto produto2 = new Produto("TV 65 Polegadas", "Eletrônico", 2500.00m, 10);
+         Produto produto2 = new Produto("TV 65 Polegadas OLED", "Eletrônico", 2500.00m, 10);
          produto2.ExibirInfo();
 
          Console.WriteLine();
-        Produto produto3 = new Produto("Smartphone Iphone", "Eletrônico");
+        Produto produto3 = new Produto("Smartphone Iphone Pro Max", "Eletrônico");
           produto3.ExibirInfo();
 
          Console.WriteLine();
