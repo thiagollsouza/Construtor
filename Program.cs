da462bf8b2b15e9fce7d07299addc969335f59be
Thiago Luis <thiagoviips@hotmail.com>
2025-01-18 16:59:42 -0400

    atualizado 2025

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ namespace MyApp
          produto2.ExibirInfo();
 
          Console.WriteLine();
-        Produto produto3 = new Produto("Smartphone samsung.", "Eletrônico");
+        Produto produto3 = new Produto("Smartphone Iphone", "Eletrônico");
           produto3.ExibirInfo();
 
          Console.WriteLine();
